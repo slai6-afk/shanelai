f4853dc1dd55610a8ceb6177987efd2d5718d928
Shane <shane@mac.mynetworksettings.com>
2025-11-07 11:46:29 -0500

    fix: correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/shanelai/', 
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
