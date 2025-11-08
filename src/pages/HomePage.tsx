fb0c1e1133f163ddd702535f4389020e3ef22f0a
slai6-afk <slai6@pratt.edu>
2025-11-08 13:32:41 -0500

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -43,7 +43,7 @@ export function HomePage() {
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-            style={{ marginTop: '-20px' }}
+           
           >
             <h1 
               className="mb-12 max-w-5xl"
